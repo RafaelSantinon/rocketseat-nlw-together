5fc6b6c3286710488b7337c06fb51d9e6145ee42
Rafael Santinon <rafaelns94@outlook.com>
2021-06-21 01:55:16 -0300

    first day

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.get("/test", (request, response) => {
+  return response.send("Olá NLW");
+});
+
+app.post("/test-post", (resquest, response) => {
+  return response.send("Olá");
+})
+
+app.listen(3000, () => {
+  console.log("Server is running!")
+});
